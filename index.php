e1669fe5e6f6aa99aff5fb85649e835e709880e3
Jack Platten <jack@plttn.me>
2016-02-22 19:30:49 +0000

    Add boilerplate

--- index.php
+++ index.php
@@ -1,3 +1,33 @@
+<?php 
+    $card = ["imageName" => "",
+            "score" => "",
+            "suit" => "",
+            "rank" => ""];
+    //indexed array
+    $deck = [];
+
+    $player1 = ["imageName"=>"",
+                "name" => ""];
+    $player2 = ["imageName"=>"",
+                "name" => ""];
+    $player3 = ["imageName"=>"",
+                "name" => ""];
+    $player4 = ["imageName"=>"",
+                "name" => ""]; 
+                
+    $table = [$player1, $player2, $player3, $player4];
+    
+    $hands = ["player" => null,
+                "cards" => [],
+                "score" => ""
+            ];
+    
+    $game = ["players" => $table,
+            "hands" => [],
+            ];
+    
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
