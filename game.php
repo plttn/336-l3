12c352dbdaa178e240db45065caa8070dec471a9
Jack Platten <jack@plttn.me>
2016-02-24 11:24:48 -0800

    Add card generation

--- game.php
+++ game.php
@@ -1,10 +1,46 @@
-<?php 
-    // $card = ["imageName" => "",
-    //         "score" => "",
-    //         "suit" => "",
-    //         "rank" => ""];
+<?php
+    $card = ["imageName" => "",
+            "score" => "",
+            "suit" => "",
     //indexed array
+
+
     $deck = [];
+
+    //diamonds
+    $suitName = "diamonds";
+    for ($i=1; $i <= 13; $i++) {
+        $card["imageName"] = "/Labs/336-l3/assets/cards/" . $suitName . $i . ".png";
+        $card["score"] = $i;
+        $card["suit"] = $suitName;
+    }
+
+    //clubs
+    $suitName = "clubs";
+    for ($i=1; $i <= 13; $i++) {
+        $card["imageName"] = "/Labs/336-l3/assets/cards/" . $suitName . $i . ".png";
+        $card["score"] = $i;
+        $card["suit"] = $suitName;
+    }
+
+    //hearts
+    $suitName = "hearts";
+    for ($i=1; $i <= 13; $i++) {
+        $card["imageName"] = "/Labs/336-l3/assets/cards/" . $suitName . $i . ".png";
+        $card["score"] = $i;
+        $card["suit"] = $suitName;
+    }
+
+    //spades
+    $suitName = "spades";
+    for ($i=1; $i <= 13; $i++) {
+        $card["imageName"] = "/Labs/336-l3/assets/cards/" . $suitName . $i . ".png";
+        $card["score"] = $i;
+        $card["suit"] = $suitName;
+    }
+
+
+
 
     $player1 = ["imageName"=>"",
                 "name" => $_POST["p1"]];
@@ -14,18 +50,18 @@
                 "name" => $_POST["p3"]];
     $player4 = ["imageName"=>"",
                 "name" => $_POST["p4"]];
-                
+
     $table = [$player1, $player2, $player3, $player4];
-    
+
     $hands = ["player" => null,
                 "cards" => [],
                 "score" => ""
             ];
-    
+
     $game = ["players" => $table,
             "hands" => [],
             ];
-    
+
 ?>
 
 <!DOCTYPE html>
@@ -34,7 +70,7 @@
         <title> </title>
     </head>
     <body>
-<?php 
+<?php
     var_dump($table);
 ?>
     </body>
