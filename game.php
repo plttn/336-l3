2f2aa2cb794437aebcf310d36b1b751518966367
Jack Platten <jack@plttn.me>
2016-02-26 05:23:11 +0000

    Add basic hand functionality

--- game.php
+++ game.php
@@ -1,29 +1,61 @@
 <?php
-    $card = ["imageName" => "",
-            "score" => "",
-            "suit" => "",
-            "rank" => ""];
-    //indexed array
-    $deck = [];
-    $player1 = ["imageName"=>"",
-                "name" => ""];
-    $player2 = ["imageName"=>"",
-                "name" => ""];
-    $player3 = ["imageName"=>"",
-                "name" => ""];
-    $player4 = ["imageName"=>"",
-                "name" => ""];
-    $table = [$player1, $player2, $player3, $player4];
-    $hands = ["player" => null,
-                "cards" => [],
-                "score" => ""
-            ];
-    $game = ["players" => $table,
-            "hands" => [],
-            ];
-?>
 
-<!DOCTYPE html>
+$player1hand = array();
+$player2hand = array();
+$player3hand = array();
+$player4hand = array();
+
+
+$deck = array();
+for ($i = 1; $i <= 52; $i++ ) {
+  $deck[] = $i;
+}
+//print_r($deck);
+shuffle($deck);
+
+for ($i = 1; $i <= rand(4,6); $i++) {
+  $player1hand[] = array_pop($deck);
+}
+for ($i = 1; $i <= rand(4,6); $i++) {
+  $player2hand[] = array_pop($deck);
+}
+for ($i = 1; $i <= rand(4,6); $i++) {
+  $player3hand[] = array_pop($deck);
+}
+for ($i = 1; $i <= rand(4,6); $i++) {
+  $player4hand[] = array_pop($deck);
+}
+
+var_dump($player1hand);
+
+
+
+
+for ($i = 0; $i < count(${"player" . 1 . "hand"}); $i++) {
+    //prints the hand
+    
+    $cardSuit;
+
+    $suits = array("clubs", "diamonds", "hearts", "spades");
+    
+    $cardSuitValue = floor(${"player" . 1 . "hand"}[$i]/13);
+    if ((${"player" . 1 . "hand"}[$i]/13) == floor(${"player" . 1 . "hand"}[$i]/13)) {
+        $cardSuitValue = $cardSuitValue - 1;
+    }
+    
+    $cardSuit = $suits[$cardSuitValue];
+    
+    $cardValue = ${"player" . 1 . "hand"}[$i] % 13;
+    if($cardValue==0){
+        $cardValue = 13;
+    }
+    echo "<img src=assets/cards/". $cardSuit . "/" . $cardValue . ".png>";
+}
+
+
+ ?>
+ 
+ <!DOCTYPE html>
 <html>
     <head>
         <title> </title>
@@ -33,51 +65,3 @@
 ?>
     </body>
 </html>
-
-<?php
-$deck = array();
-for ($i = 1; $i <= 52; $i++ ) {
-  $deck[] = $i;
-}
-//print_r($deck);
-shuffle($deck);
-echo "<hr>";
-print_r($deck);
-$card = array_pop($deck);
-echo $card;
-$suit = array("clubs", "diamonds", "hearts", "spades");
-$cardSuit = $suit[floor($card / 13)];
-$randomCard = rand(1,13);
-$cardValue = $card % 13;
-if ($cardValue == 0) {
-    $cardValue = 13;
-}
-echo "<img src=cards/$cardSuit/$cardValue.png>";
-$deck = array();
-for ($i =1; $i <=52; $i++){
-    $deck[] =$i;
-}
-//shuffles the deck
-shuffle($deck); 
-echo "<hr>";
-
-//prints the deck
-print_r($deck); 
-
-//takes the selected card from previous off deck
-$card = array_pop($deck); 
-echo $card;
-$suit = array("clubs", "diamonds", "hearts", "spades");
-$cardSuit = $suit[floor($card/13)];
-$randomCard = rand(1,13);
-$vardValues = $card % 13;
-if($cardValue==0){
-    $cardValue =13;
-}
-
-echo $player1;
-echo $player2;
-echo $player3;
-echo $player4;
-//echo "<img src=cards/$cardSuit/$cardValue.png>"
- ?>
